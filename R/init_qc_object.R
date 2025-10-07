c51c71671fc09010476ceb6917b41925deea5775
Ido Hatam <ihatam@BIOL-67714-L.local>
2025-10-07 15:23:05 -0700

    Generated the initiator function the returns an object of LongReadQC class. The object has the file slot and metadeta slot filled.

---
+++ R/init_qc_object.R
@@ -0,0 +1,14 @@
+#' This is an internal function that is called by the main vizualization workflow function.
+#' It initiates the LongReadQC objects and fills in the files and metadata slots. 
+#' @keywords internal
+#' @noRd
+#' 
+
+.init_qc_object <- function(files) {
+  new("LongReadQC",
+      files = files,
+      metadata = list(
+        analysis_date = Sys.time(),
+        n_files = length(files)
+      ))
+}
