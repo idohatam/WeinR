7a9180f12fcaf77143839990b8e933b217ca64d5
sarah-bowker <sarahbowker00@gmail.com>
2025-10-21 22:02:43 -0700

    Changed import function to handle one path at a time instead of multiple paths. Return a named list with file path and xstring object

--- R/ImportFile.R
+++ R/ImportFile.R
@@ -5,16 +5,16 @@
 #    -A list of QualityScaledDNAStringSet objects, a list item per file
 #  Stops with an error if the file type isn't supported or file doesn't exist
 
-ImportFile <- function(filePaths) {
-  results <- lapply(filePaths, function(f) {
-    infile <- CheckFile(f)
+ImportFile <- function(filePath) {
+    infile <- CheckFile(filePath)
     if (infile %in% c("fastq", "fastq.gz")) {
-      Biostrings::readQualityScaledDNAStringSet(f)
+      Output <- Biostrings::readQualityScaledDNAStringSet(filePath)
     } else {
-      bam <- Rsamtools::scanBam(f)[[1]]
-      Biostrings::QualityScaledDNAStringSet(bam$seq, bam$qual)
+      bam <- Rsamtools::scanBam(filePath)[[1]]
+      Output <- Biostrings::QualityScaledDNAStringSet(bam$seq, bam$qual)
     }
-  })
-  names(results) <- basename(filePaths)
-  return(results)
+  
+  result <- list(Output)
+  names(result) <- filePath
+  return(result)
 }
